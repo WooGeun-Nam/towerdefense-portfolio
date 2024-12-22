3a8db7e95d5097b6202da12bb6546a029c1a4806
WooGeun-Nam <82949734+WooGeun-Nam@users.noreply.github.com>
2024-12-23 00:58:58 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import GamePortfolio from '../components/GamePortfolio';
 
-export default function Page() {
+export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 py-8">
       <GamePortfolio />
