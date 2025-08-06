5b47c22c2fdb41eca61b1612134c829dec6658a9
WooGeun-Nam <82949734+WooGeun-Nam@users.noreply.github.com>
2025-08-06 18:23:22 +0900

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import React from "react"; // React import 추가
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "RogueTower", // 제목 수정
+  description: "게임 기획서", // 설명 수정
+  icons: {
+    icon: "./logo.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body className={`${inter.className} min-h-screen bg-gray-100`}>
+        {children}
+      </body>
+    </html>
+  );
+}
