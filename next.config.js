d7bbd0e3fe96180af40a349c7bfce47fa31103be
WooGeun-Nam <rolru7686@gmail.com>
2025-09-25 06:03:24 +0900

    config mod

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: isProd ? '/towerdefense-portfolio' : '',
-  assetPrefix: isProd ? '/towerdefense-portfolio/' : '',
+  basePath: isProd ? "/towerdefense-portfolio" : "",
+  assetPrefix: isProd ? "/towerdefense-portfolio" : "",
   trailingSlash: true,
-  output: 'export', // 추가된 부분
+  output: "export", // 추가된 부분
 };
 
 module.exports = nextConfig;
