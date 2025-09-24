5f0c8e5af37bfbf268d9d557a227b11a9ac99261
WooGeun-Nam <WooGeun-Nam@users.noreply.github.com>
2025-09-24 21:45:29 +0000

    deploy: 8e4a38bc1547fc339b55b41f65a96a8bc26e9faa

--- _next/static/chunks/app/projects/detectfood/page-0d45b8c7cd3a5d1b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[45,951,582,292,593,680,850,406,835],{3521:function(){}},function(n){n.O(0,[971,864,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
