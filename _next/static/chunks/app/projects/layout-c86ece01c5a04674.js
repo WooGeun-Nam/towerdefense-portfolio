f462fb10dbe975c4bca5abedbb19622089262563
WooGeun-Nam <WooGeun-Nam@users.noreply.github.com>
2025-09-24 21:30:38 +0000

    deploy: 4e3c36a71e65e314685f818e94f7775a02b53ddb

--- _next/static/chunks/app/projects/layout-c86ece01c5a04674.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[570],{1164:function(e,r,t){Promise.resolve().then(t.bind(t,3891))},3891:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return o}});var n=t(7437);function o(){return(0,n.jsx)("header",{className:"fixed top-0 inset-x-0 z-50 bg-white/80 backdrop-blur border-b",children:(0,n.jsxs)("nav",{className:"mx-auto max-w-6xl px-4 h-14 flex items-center justify-between",children:[(0,n.jsx)("a",{href:"/portfolio",className:"font-semibold",children:"WooGeun Nam"}),(0,n.jsx)("div",{className:"flex gap-5 text-sm",children:(0,n.jsx)("a",{href:"/portfolio",className:"px-1 transition-colors text-gray-600 hover:text-gray-900",children:"Home"})})]})})}},622:function(e,r,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),a=Object.prototype.hasOwnProperty,i=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function c(e,r,t){var n,s={},c=null,l=null;for(n in void 0!==t&&(c=""+t),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(l=r.ref),r)a.call(r,n)&&!f.hasOwnProperty(n)&&(s[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===s[n]&&(s[n]=r[n]);return{$$typeof:o,type:e,key:c,ref:l,props:s,_owner:i.current}}r.Fragment=s,r.jsx=c,r.jsxs=c},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,864,744],function(){return e(e.s=1164)}),_N_E=e.O()}]);
