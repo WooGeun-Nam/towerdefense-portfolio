3c121b637a9b9a7b86ad60766bf50a50779cf9d3
WooGeun-Nam <WooGeun-Nam@users.noreply.github.com>
2025-09-24 21:51:45 +0000

    deploy: a95c855c67f4b5f6c0e584aca587049be892ba5a

--- _next/static/chunks/app/projects/kiosk/page-3128b5b012b8db46.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[593,951,582,292,45,680,406,850,835],{3521:function(){}},function(n){n.O(0,[971,864,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
