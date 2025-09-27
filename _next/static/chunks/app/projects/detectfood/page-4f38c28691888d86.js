83dea202432314205c18b92d5a95a03178600255
WooGeun-Nam <WooGeun-Nam@users.noreply.github.com>
2025-09-27 18:55:12 +0000

    deploy: 30a733d672bbbf5ba077f721539464260a90625f

--- _next/static/chunks/app/projects/detectfood/page-4f38c28691888d86.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[45,951,582,292,680,593,406,850,835],{5448:function(e,t,r){Promise.resolve().then(r.bind(r,2148))},2148:function(e,t,r){"use strict";r.r(t),r.d(t,{default:function(){return c}});var n=r(7437),o=r(2265);let s=()=>"/towerdefense-portfolio";function c(e){let{src:t,...r}=e,[c,f]=(0,o.useState)("");if((0,o.useEffect)(()=>{f(s())},[]),!t)return(0,n.jsx)("img",{...r});let u=t.startsWith("http")?t:"/".concat(t.replace(/^(\.\/+|\.\.\/+)+/,"").replace(/^\/+/,"")),i=u.startsWith("http")?u:"".concat(c).concat(u);return(0,n.jsx)("img",{src:i,...r})}},622:function(e,t,r){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=r(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),c=Object.prototype.hasOwnProperty,f=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function i(e,t,r){var n,s={},i=null,a=null;for(n in void 0!==r&&(i=""+r),void 0!==t.key&&(i=""+t.key),void 0!==t.ref&&(a=t.ref),t)c.call(t,n)&&!u.hasOwnProperty(n)&&(s[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===s[n]&&(s[n]=t[n]);return{$$typeof:o,type:e,key:i,ref:a,props:s,_owner:f.current}}t.Fragment=s,t.jsx=i,t.jsxs=i},7437:function(e,t,r){"use strict";e.exports=r(622)}},function(e){e.O(0,[971,864,744],function(){return e(e.s=5448)}),_N_E=e.O()}]);
