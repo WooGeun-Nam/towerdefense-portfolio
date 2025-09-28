e73784f5d58944edbb7c128581c5ea930367491d
WooGeun-Nam <rolru7686@gmail.com>
2025-09-28 23:19:09 +0900

    fix ico

--- app/portfolio/layout.tsx
+++ app/portfolio/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] }); // 기존 그대로
 export const metadata: Metadata = {
   title: "WooGeun Nam",
   description: "웹 포트폴리오",
-  icons: { icon: "/Logo.ico" },
+  icons: { icon: "./../Logo.ico" },
 };
 
 export default function PortfolioLayout({
